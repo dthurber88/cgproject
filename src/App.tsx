fdabff1aa6f33a42d0cc68098aac7f5b48da46fd
dthurber88 <dthurber88@gmail.com>
2024-12-25 23:42:26 -0500

    code formatting

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,42 @@ import "./App.css";
 import useGetQuote from "./hooks/useGetQuote";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const { data, isLoading, error } = useGetQuote("https://api.api-ninjas.com/v1/quotes");
-  
-  const catchVariable = isLoading ? data : error;
-  console.log(catchVariable)
+    const [count, setCount] = useState(0);
+    const { data, isLoading, error } = useGetQuote(
+        "https://api.api-ninjas.com/v1/quotes"
+    );
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          The count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    const catchVariable = isLoading ? data : error;
+    console.log(catchVariable);
+
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 2)}>
+                    The count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
 export default App;
--- src/hooks/useGetQuote.tsx
+++ src/hooks/useGetQuote.tsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 function useGetQuote(url: string) {
     const [data, setData] = useState<Quote | undefined>(undefined);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-    
+
     type Quote = {
         author?: string;
         quote?: string;
         category?: string;
     };
-    
+
     // type ApiResponse = {
     //     results?: Quote[]
     // }
 
     useEffect(() => {
         if (!url) return;
-        
+
         const getQuote = async () => {
             setIsLoading(true);
             setError(null);
@@ -31,26 +31,24 @@ function useGetQuote(url: string) {
                 // }
 
                 // const result = await response.json();
-                const result: Quote = 
-                    {
-                    author: "Gary Ross",
-                    category: "imagination",
-                    quote: "Ultimately, so much Dr. Seuss is about empowerment. He invites us to disappear into our imagination."
-                    }
-                ;
+                const result: Quote = {
+                    author: "Steve Jobs",
+                    category: "motivational",
+                    quote: "Life can be much broader once you discover one simple fact: Everything around you that you call life was made up by people that were no smarter than you and you can change it. Once you learn that, you'll never be the same again.",
+                };
                 setData(result);
             } catch (err: any) {
                 setError(err.message);
-                console.log(err.message)
+                console.log(err.message);
             } finally {
                 setIsLoading(false);
             }
-        }
+        };
 
         getQuote();
-    }, [])
+    }, []);
 
     return { data, isLoading, error };
 }
 
-export default useGetQuote
+export default useGetQuote;
